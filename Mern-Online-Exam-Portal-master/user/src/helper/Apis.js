bc45e3b6a8d382ea702b388800834e2602c3b766
Vamsi <nr777668@gmail.com>
2025-07-21 09:43:20 +0530

    first commit

---
+++ Mern-Online-Exam-Portal-master/user/src/helper/Apis.js
@@ -0,0 +1,31 @@
+const environment = process.env.NODE_ENV;
+
+const apis = {
+  BASE_LOCAL_URL:environment==='development'?'http://localhost:3001':'',
+  BASE : environment==='development'?'http://localhost:5000':'',
+  LOGIN : "/api/v1/login",
+  GET_USER_DETAILS: "/api/v1/user/details",
+  STUDENT_REGISTER: "/api/v1/public/register",
+  GET_ALL_SUBJECT: "/api/v1/user/getAllSubjects",
+  ADD_QUESTION : '/api/v1/user/addQuestion',
+  SEARCH_QUESTION : '/api/v1/user/searchQuestion',
+  CHANGE_QUESTION_STATUS : '/api/v1/user/changeQuestionStatus',
+  GET_QUESTION_BY_ID:'/api/v1/user/getQuestionAnswer',
+  GET_ANSWER : '/api/v1/user/getAnswer',
+  UPDATE_QUESTION : '/api/v1/user/updateQuestion',
+  CREATE_QUESTION : '/api/v1/user/createTest',
+  GET_ALL_TEST : '/api/v1/user/getAllTest',
+  GET_ALL_TEST_STUDNET:'/api/v1/user/getAllTestStudent',
+  STUDENT_TEST_REGISTER : '/api/v1/user/testRegistration',
+  GET_UPCOMING_TESTS_STUDENT : '/api/v1/user/getUpcomingTests',
+  GET_TEST_DETAILS_BY_ID : '/api/v1/user/getTestById',
+  START_TEST : '/api/v1/user/startTest',
+  GET_QUESTIONS_TAKETEST : '/api/v1/user/getQuenStarttime',
+  SAVE_ANSWER : '/api/v1/user/saveAnswer',
+  END_TEST : '/api/v1/user/endTest',
+  GET_ALL_COMPLETED_TEST_STUDENT : '/api/v1/user/getAllCompletedTest',
+  GET_TEST_RESULT_STUDENT:'/api/v1/user/getResultMainDetailsByTestId',
+  GET_RESULT_QUESTIONS_STUDENTS : '/api/v1/user/getQuestionAnswerByIds'
+}
+
+export default apis;
